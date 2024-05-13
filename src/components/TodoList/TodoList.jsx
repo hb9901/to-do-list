5e777a2138fc75a525948964cd76b96ce52dd9ab
hb9901 <hb9901@gmail.com>
2024-05-13 19:08:03 +0900

    update: todoList 상세 기능 구현

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,13 +2,12 @@ import Card from "../common/Card/Card";
 import "./TodoList.css";
 
 export default function TodoList({ title, list }) {
-  console.log(list)
   return (
     <div className="todoList-wrapper">
       <div className="todoList-title">{title}</div>
       {
         list.map((todo) => {
-          <Card id={todo.id} title={todo.title} content={todo.content} />;
+          return <Card key={todo.id} title={todo.title} content={todo.content} />;
         })}
     </div>
   );
