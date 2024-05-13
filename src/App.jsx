f3825d017c50f4d119c414e60505768353326c75
hb9901 <hb9901@gmail.com>
2024-05-13 14:14:26 +0900

    update: 파일 구조 생성

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='layout-wrapper'>
+        <header></header>
+        <div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
